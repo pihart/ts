ac836558b86623d86e33acf64e5100a8c61ac14c
pihart <avi.mehra1@gmail.com>
2021-01-31 19:34:07 -0800

    Add type NonEmptyArray
    Resolves https://github.com/pihart/ts/issues/8

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -1,2 +1,13 @@
 export type Async<T = void> = T | Promise<T>;
 export type Predicate<T> = (value: T) => boolean;
+
+/**
+ * Arrays with at least one element
+ *
+ * @typeparam T The type of the first element
+ * @typeparam U The type of the remaining elements, if different from T
+ *
+ * @warn Typescript ^4.1.3 allows you to pull off elements from empty arrays, even with strict mode enabled.
+ * This mainly protects arguments to functions.
+ */
+export type NonEmptyArray<T, U = T> = [T, ...U[]];
