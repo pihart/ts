481ce05217c2883db4b99b6b1a3baffe080f55d7
pihart <avi.mehra1@gmail.com>
2021-03-23 12:55:32 -0700

    Use JSDoc syntax for @throws annotations
    Doesn't render correctly in typedoc but at least cooperates with IDE/editor tooling

--- src/lib/assert.ts
+++ src/lib/assert.ts
@@ -3,8 +3,9 @@ import { CustomError } from "./error";
 export class AssertionError extends CustomError {}
 
 /**
- * Asserts `bool`
- * @throws `errorConstructor`
+ * Asserts {@param bool}
+ * @param errorConstructor A constructor for the thrown error class
+ * @throws {@param errorConstructor}
  */
 export function Assert(
   bool: boolean,
