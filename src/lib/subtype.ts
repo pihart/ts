7d3b295162e61b630a469b3d01523c7e828306f5
pihart <avi.mehra1@gmail.com>
2021-03-23 19:32:45 -0700

    Add type RequireSubType

--- src/lib/subtype.ts
+++ src/lib/subtype.ts
@@ -5,8 +5,16 @@ import { TestedType } from "./misc";
  * otherwise type `false`.
  */
 export type IsSubType<A, B> = A extends B ? true : false;
+
 /**
  * Type {@typeparam A} if {@typeparam A} is a subtype of {@typeparam B};
  * otherwise type `never`.
  */
 export type SubType<A, B> = TestedType<A, IsSubType<A, B>>;
+
+/**
+ * A stricter version of [[`IsSubType`]].
+ * Type `true` if {@typeparam A} is a subtype of {@typeparam B};
+ * compile-time error otherwise.
+ */
+export type RequireSubType<A extends B, B> = IsSubType<A, B>
