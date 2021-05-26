ac3cec705dc6060775b9576855ad950a52ac5b07
pihart <avi.mehra1@gmail.com>
2021-05-26 14:08:18 -0700

    Add `IsEqual`

--- src/lib/subtype.ts
+++ src/lib/subtype.ts
@@ -3,6 +3,9 @@ import { TestedType } from "./misc";
 /**
  * Type `true` if {@typeparam A} is a subtype of {@typeparam B};
  * otherwise type `false`.
+ *
+ * @see [[`IsEqual`]] for a bidirectional variant.
+ * @see [[`RequireSubType`]] for a stricter variant.
  */
 export type IsSubType<A, B> = A extends B ? true : false;
 
@@ -17,4 +20,11 @@ export type SubType<A, B> = TestedType<A, IsSubType<A, B>>;
  * Type `true` if {@typeparam A} is a subtype of {@typeparam B};
  * compile-time error otherwise.
  */
-export type RequireSubType<A extends B, B> = IsSubType<A, B>
+export type RequireSubType<A extends B, B> = IsSubType<A, B>;
+
+/**
+ * Type `true` if {@typeparam A} :< {@typeparam B} && {@typeparam B} :< {@typeparam A}
+ *
+ * @see [[`IsSubType`]] for a unidirectional variant.
+ */
+export type IsEqual<A, B> = IsSubType<true, IsSubType<A, B> & IsSubType<B, A>>;
